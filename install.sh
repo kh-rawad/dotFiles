4914724711353731cdc771e34e9efce5b9b0def2
rawad kharma <kh.rawad@gmail.com>
2021-07-20 20:56:03 +0300

    SYS: add copy to dotfiles

--- install.sh
+++ install.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
 cp .bashrc ~/.bashrc
-cp -r .vim/ !/.vim
+cp -r .vim/ ~/.vim
 cp .vimrc ~/.vimrc
+
+if [ ! -d dotfiles ]; then
+echo "creating dotFiles folder"
+mkdir ~/dotfiles
+fi
+
+cp aliases ~/dotfiles
+cp exports ~/dotfiles
+cp functions ~/dotfiles
+
 
 source ~/.bashrc
