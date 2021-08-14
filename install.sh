9ec7e45874f08d48d62321e2fe5c625e03e0aa08
Rawad Kharma <kh.rawad@gmail.com>
2021-08-14 08:35:43 +0000

    SYS: load bachrc in android termux

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ echo "Installing VIM Config"
 cp -f -r .vim/ ~/.vim
 cp -f .vimrc ~/.vimrc
 
-if [ -d ~dotfiles ]; then echo "DotFiles folder exsists continue"; fi
+if [ -d ~/dotfiles ]; then echo "DotFiles folder exsists continue"; fi
 if [ ! -d ~/dotfiles ]; then echo "creating dotFiles folder"; mkdir ~/dotfiles; fi
 
 echo "Installing Aliases"
@@ -16,6 +16,31 @@ cp -f exports ~/dotfiles
 echo "Installing Functions"
 cp -f functions ~/dotfiles
 
+echo "Installing .Profile";
+if [ ! -f ~/.profile ]; then echo "Creating [~/.profile] "; touch ~/.profile; fi
+grep -q \DOTFILES ~/.profile
+if [ $? -eq 1 ]; then
+    echo "Updating .profile"
+    cat <<EOT >> ~/.profile
+#[DOTFILES] execute .bashrc for non-login shells.
+if [ -n "\$BASH_VERSION" ]; then    
+    if [ -f "\$HOME/.bashrc" ]; then
+        . "\$HOME/.bashrc"
+    fi 
+fi
+#[DOTFILES]
+EOT
+else 
+    echo "please remove any [DOTFILES] lines in [~/.profile] before update" 
+fi
+
+# install dependencies
+cat <<EOT 
+# if arch linux
+# pacman -Syy & pacman -S vim neofetch powerline
+# if debian 
+# apt update & apt upgrade & apt install vim neofetch powerline
+
+# Restart your session OR source .bashrc
+EOT
 sleep 2
-clear
-source ~/.bashrc
