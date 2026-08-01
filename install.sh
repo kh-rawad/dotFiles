e35e420cf218586fb1e322704c8125118b18be1b
rawad <rawad@yoga>
2026-08-01 23:19:37 +0300

    fix: resolve formatting issues and improve readability in install.sh

--- install.sh
+++ install.sh
@@ -3,60 +3,60 @@
 source "$(dirname "$0")/setEnv"
 
 build_localdist() {
-LOCALDIST_DIR="./localdist"
-LOCALDIST_DOTFILES_DIR="${LOCALDIST_DIR}/.config/dotfiles"
+  LOCALDIST_DIR="./localdist"
+  LOCALDIST_DOTFILES_DIR="${LOCALDIST_DIR}/.config/dotfiles"
 
-rm -rf "$LOCALDIST_DIR"
-mkdir -p "$LOCALDIST_DOTFILES_DIR"
+  rm -rf "$LOCALDIST_DIR"
+  mkdir -p "$LOCALDIST_DOTFILES_DIR"
 
-cp -f SHELLS/BASH/bashrc "${LOCALDIST_DIR}/.bashrc"
-cp -f SHELLS/ZSH/zshrc "${LOCALDIST_DIR}/.zshrc"
-cp -f aliases "${LOCALDIST_DOTFILES_DIR}/aliases"
-cp -f exports "${LOCALDIST_DOTFILES_DIR}/exports"
-cp -f functions "${LOCALDIST_DOTFILES_DIR}/functions"
+  cp -f SHELLS/BASH/bashrc "${LOCALDIST_DIR}/.bashrc"
+  cp -f SHELLS/ZSH/zshrc "${LOCALDIST_DIR}/.zshrc"
+  cp -f aliases "${LOCALDIST_DOTFILES_DIR}/aliases"
+  cp -f exports "${LOCALDIST_DOTFILES_DIR}/exports"
+  cp -f functions "${LOCALDIST_DOTFILES_DIR}/functions"
 
-export LOCALDIST_EXPORTS="${LOCALDIST_DOTFILES_DIR}/exports"
-export LOCALDIST_ALIASES="${LOCALDIST_DOTFILES_DIR}/aliases"
-export LOCALDIST_BASHRC="${LOCALDIST_DIR}/.bashrc"
-export LOCALDIST_ZSHRC="${LOCALDIST_DIR}/.zshrc"
+  export LOCALDIST_EXPORTS="${LOCALDIST_DOTFILES_DIR}/exports"
+  export LOCALDIST_ALIASES="${LOCALDIST_DOTFILES_DIR}/aliases"
+  export LOCALDIST_BASHRC="${LOCALDIST_DIR}/.bashrc"
+  export LOCALDIST_ZSHRC="${LOCALDIST_DIR}/.zshrc"
 }
 
 echo "Detected system: $SYSTEM_INFO"
 if [[ -z "${SYSTEM_INFO:-}" ]]; then
-echo "System information not detected. Please set SYSTEM_INFO in setEnv file."
-exit 1
+  echo "System information not detected. Please set SYSTEM_INFO in setEnv file."
+  exit 1
 fi
 
 # install dependencies
 if [[ -z "${PACKAGING_START:-}" ]]; then
-echo "Do you want to install dependencies? (Yes/No)"
-read -r install_deps
+  echo "Do you want to install dependencies? (Yes/No)"
+  read -r install_deps
 else
-install_deps="No"
+  install_deps="No"
 fi
 
 echo "################################################################"
 
 if [[ ! $install_deps =~ ^([Yy][Ee][Ss]|[Yy])$ ]]; then
-echo "Skipping dependencies installation."
+  echo "Skipping dependencies installation."
 else
-echo "Installing Dependencies"
-if is_termux; then
-if ! pkg update -y && pkg install -y vim curl wget git zsh tmux nodejs-lts stow unzip; then
-echo "Failed to install dependencies"
-exit 1
-fi
-elif [[ $OSTYPE == 'darwin'* ]]; then
-if ! brew install vim curl wget git tmux stow; then
-echo "Failed to install dependencies"
-exit 1
-fi
-elif grep -qiE "debian|buntu|mint" /etc/*release; then
-if ! sudo apt update -y && sudo apt install -y vim curl wget git zsh unzip stow bison libevent-dev; then
-echo "Failed to install dependencies"
-exit 1
-fi
-fi
+  echo "Installing Dependencies"
+  if is_termux; then
+    if ! pkg update -y && pkg install -y vim curl wget git zsh tmux nodejs-lts stow unzip; then
+      echo "Failed to install dependencies"
+      exit 1
+    fi
+  elif [[ $OSTYPE == 'darwin'* ]]; then
+  if ! brew install vim curl wget git tmux stow; then
+    echo "Failed to install dependencies"
+    exit 1
+  fi
+  elif grep -qiE "debian|ubuntu|mint" /etc/*release; then
+    if ! sudo apt update -y && sudo apt install -y vim curl wget git zsh unzip stow bison libevent-dev; then
+      echo "Failed to install dependencies"
+      exit 1
+    fi
+  fi
 
 fi
 
@@ -67,28 +67,28 @@ echo "################################################################"
 build_localdist
 
 for app in ./APPS/*; do
-echo ">>> Installing $app"
-# shellcheck disable=SC1090
-if source "$app"; then
-echo "<<< Finished installing $app"
-else
-echo "<<< Failed installing $app (exit code: $?)"
-fi
-echo -e "\n---------------------------------------------------------------\n"
+  echo ">>> Installing $app"
+  # shellcheck disable=SC1090
+  if source "$app"; then
+    echo "<<< Finished installing $app"
+  else
+    echo "<<< Failed installing $app (exit code: $?)"
+  fi
+  echo -e "\n---------------------------------------------------------------\n"
 done
 
 echo "################################################################"
-echo "running prefrences scripts"
+echo "running preferences scripts"
 echo "################################################################"
 for pref in ./PREFS/*; do
-echo ">>> Running $pref"
-# shellcheck disable=SC1090
-if source "$pref"; then
-echo "<<< Finished running $pref"
-else
-echo "<<< Failed running $pref (exit code: $?)"
-fi
-echo -e "\n---------------------------------------------------------------"
+  echo ">>> Running $pref"
+  # shellcheck disable=SC1090
+  if source "$pref"; then
+    echo "<<< Finished running $pref"
+  else
+    echo "<<< Failed running $pref (exit code: $?)"
+  fi
+  echo -e "\n---------------------------------------------------------------"
 done
 echo "################################################################"
 echo "Installing dotfiles"
